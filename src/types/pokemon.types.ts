63efab33700ec5cf90ae54328138b6d59533f229
SilverMichael <emeraudesnake@yahoo.com>
2025-04-30 16:06:53 +0300

    up(types): add pokemon types and typecolor

--- src/types/pokemon.types.ts
+++ src/types/pokemon.types.ts
@@ -37,3 +37,32 @@ export interface PokemonDetail {
         legacy: string
     }
 }
+
+export const POKEMON_TYPES = [
+    'normal', 'fire', 'water', 'electric', 'grass',
+    'ice', 'fighting', 'poison', 'ground', 'flying',
+    'psychic', 'bug', 'rock', 'ghost', 'dragon',
+    'dark', 'steel', 'fairy'
+] as const;
+export type PokemonType = typeof POKEMON_TYPES[number];
+
+export const typeColors: Record<string, string> = {
+    normal: 'bg-gray-400',
+    fire: 'bg-red-500',
+    water: 'bg-blue-500',
+    electric: 'bg-yellow-400',
+    grass: 'bg-green-500',
+    ice: 'bg-blue-200',
+    fighting: 'bg-red-700',
+    poison: 'bg-purple-500',
+    ground: 'bg-yellow-600',
+    flying: 'bg-indigo-300',
+    psychic: 'bg-pink-500',
+    bug: 'bg-green-400',
+    rock: 'bg-yellow-700',
+    ghost: 'bg-purple-700',
+    dragon: 'bg-indigo-700',
+    dark: 'bg-gray-800',
+    steel: 'bg-gray-500',
+    fairy: 'bg-pink-300',
+  };
