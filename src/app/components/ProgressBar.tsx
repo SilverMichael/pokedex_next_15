9066000c484f37398bebcaf4f418fd9fbe03a681
SilverMichael <emeraudesnake@yahoo.com>
2025-05-01 10:07:35 +0300

    up (ProgressBar):  update bg-gray-color to 300

--- src/app/components/ProgressBar.tsx
+++ src/app/components/ProgressBar.tsx
@@ -11,9 +11,9 @@ export const ProgressBar = ({ value, max, color }: ProgressBarProps) => {
   const percentage = (value / max) * 100;
 
   return (
-    <Progress.Root className="h-4 w-full bg-gray-200 rounded overflow-hidden">
+    <Progress.Root className="h-4 w-full bg-gray-300 rounded overflow-hidden">
       <Progress.Indicator
-        className={`h-full ${color}`}
+        className={`h-full ${color} `}
         style={{ width: `${percentage}%`, transition: 'width 660ms cubic-bezier(0.65, 0, 0.35, 1)' }}
       />
     </Progress.Root>
