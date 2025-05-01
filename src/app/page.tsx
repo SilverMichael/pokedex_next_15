fa75f5029c0195674668723d47a905d3b08bceef
SilverMichael <emeraudesnake@yahoo.com>
2025-05-01 10:08:05 +0300

    up(Home): remove unused import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import { getPokemonList, getPokemonDetail } from '@/services/pokemon.service';
 import HomePage from './components/HomePage';
 
 
 export default async function Home() {
-  const pokemonList = await getPokemonList()
-  const pokemonDetails = await Promise.all(
-    pokemonList.results.map(pokemon => getPokemonDetail(pokemon.name))
-  )
 
   return (
     <main className="container mx-auto py-8">
-      <HomePage pokemonDetails={pokemonDetails} />
+      <HomePage />
     </main>
   );
 }
